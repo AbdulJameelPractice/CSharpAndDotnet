55d1b314e878e93eb50281aa061710317f0becaf
Abdul Jameel <b.abduljameel@gmail.com>
2024-01-28 17:54:57 +0530

    introduction to tuple

--- 02_CSharpAdvanceFeatures/WarehouseManagementSystem/WarehouseManagementSystem.Business/OrderProcessor.cs
+++ 02_CSharpAdvanceFeatures/WarehouseManagementSystem/WarehouseManagementSystem.Business/OrderProcessor.cs
@@ -1,4 +1,5 @@
-﻿using WarehouseManagementSystem.Domain;
+﻿using System.Diagnostics.CodeAnalysis;
+using WarehouseManagementSystem.Domain;
 
 namespace WarehouseManagementSystem.Business
 {
@@ -31,6 +32,8 @@ namespace WarehouseManagementSystem.Business
 
         public void Process(IEnumerable<Order> orders)
         {
+            (var e, var f, var g) = new Tuple<int, int, int>(10, 20, 30);
+            
             var summaries = orders.Select(order => new
             {
                 Order = order.OrderNumber,
@@ -38,8 +41,20 @@ namespace WarehouseManagementSystem.Business
                 Total = order.LineItems.Sum(item => item.Price)
                 
             });
+            
+            
+
+            var firstSummary = summaries.First();
+
+            var firstSummaryWithTax = firstSummary with { Total = firstSummary.Total * 1.08m};
 
             var orderSummaries = summaries.OrderBy(summary => summary.Items);
+        }
+
+        public void ProcessTuple(Order order)
+        {
+            var group = (order.OrderNumber, order.LineItems, Sum: order.LineItems.Sum(p => p.Price));
+            Console.WriteLine(group.OrderNumber);
         }
     }
     
